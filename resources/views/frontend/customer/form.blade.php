74770e50c4187fbd61653ef9846c4f9c55ddcf9f
Prajwal Adhikari <84375458+prajwalbro@users.noreply.github.com>
2022-08-07 08:29:37 +0545

    fixed some  positioning

--- resources/views/frontend/customer/form.blade.php
+++ resources/views/frontend/customer/form.blade.php
@@ -1,10 +1,23 @@
 @extends ('frontend.layouts.app')
 @section('content')
     <div class="container login-section py-5">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="{{ asset('assets/images/access.png') }}" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-9">
+                <h2 class="login-right-title ">
+                    {{$campaign->name}}
+                </h2>
+            </div>
+        </div>
         <div class="row gx-5">
             <div class="col-lg-7 col-md-8">
                 <div class="login-form bg-light mt-4 p-4">
-                    <img src="{{ asset('assets/images/access.png') }}" alt="" class="img-fluid">
+                    
+                    @if(isset($campaign) && $campaign->banner)
+                    <img class="img-fluid" src="{{ asset($campaign->banner_path)}}"/>
+                @endif
                     @if(Illuminate\Support\Facades\Session::has('success'))
                         <div class="alert alert-success">
                             {{Illuminate\Support\Facades\Session::get('success')}}
@@ -13,7 +26,7 @@
                             </button>
                         </div>
                     @endif
-                    <h5 class="login_welcome">Please register to continue. </h5>
+                    <h5 class="login_welcome">Register now for Appointments</h5>
                     <form method="POST" name="enq" action="{{ route('customerform.store') }}">
                         @csrf
                         <input type="hidden" name="campaign_id" id="" value="{{$campaign->id}}">
@@ -77,10 +90,11 @@
                                 <input required="required" placeholder="Enter Test Score " class="form-control"
                                     name="test_score" type="text">
                         </div>
+                        </div>
 
                         <div class="row">
-                            <div class="form-group col-12">
-                                <div class="col-12">
+                            <div class="form-group col-md-12">
+                                
                                     <select class="form-select form-control" name="preffered_location" aria-label="Default select example">
                                         <option selected disabled>Registering For</option>
                                         <option value="baneshwor">NZ Admission Day New Baneshor (24 August)</option>
@@ -91,10 +105,10 @@
                                                 <option value="putalisadak">NZ Admission Day Putalisadak (30 August)</option>
 
                                       </select>
-                                </div>
+                                
                             </div>
                         </div>
-
+                        <div class="row">
                         <div class="col-lg-12">
                             <button type="submit" title="Submit Your Message!" class="btn btn-default" name="submit"
                                 value="Submit">Submit</button>
@@ -106,14 +120,10 @@
         </div>
         <div class="col-lg-5 col-md-4 d-flex align-items-center pl-5">
             <div class="login-right">
-                <h2 class="login-right-title ">
-                    {{$campaign->name}}
-                </h2>
+               
 
                 <div class="login-right-list pt-4 ">
-                    @if(isset($campaign) && $campaign->banner)
-                        <img class="img-fluid" src="{{ asset($campaign->thumbnail_path)}}"/>
-                    @endif
+                   
                     {!! $campaign->description !!}
 
                 </div>
